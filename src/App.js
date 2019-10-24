ce9e111a759b4830cb30eb6970bfe16b87b75d71
MainDesktop_KimJunSeob <wnstjq616@gmail.com>
2019-10-24 23:44:48 +0900

    기본연습

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+function Food({ favorite }) {
+  return <h1>I like {favorite}</h1>;
+}
+
 function App() {
-  return <div>React 초기화</div>;
+  return (
+    <div>
+      <h1>안녕하세요</h1>
+      <Food favorite="김치"></Food>
+      <Food favorite="라면"></Food>
+    </div>
+  );
 }
 
 export default App;
