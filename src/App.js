c7802dcafbdb50eefd7d3a316eaf2dfbfd69fedf
MainDesktop_KimJunSeob <wnstjq616@gmail.com>
2019-10-30 07:13:11 +0900

    Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,73 +1,34 @@
 import React from "react";
 import propTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "김치",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    id: 2,
-    name: "삽겹살",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.9
-  },
-  {
-    id: 3,
-    name: "비빔밥",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.8
-  },
-  {
-    id: 4,
-    name: "돈까스",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 5.5
-  },
-  {
-    id: 5,
-    name: "김밥",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.7
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
+  componentDidMount() {
+    console.log("conponentDidMount");
   }
-];
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}></img>
-    </div>
-  );
-}
-
-// Check
-Food.propTypes = {
-  name: propTypes.string.isRequired,
-  picture: propTypes.string.isRequired,
-  rating: propTypes.number
-};
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <h1>the number is: {this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
