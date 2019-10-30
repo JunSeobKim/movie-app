28e965e2719053d8ba010cd79a761313b393ed02
MainDesktop_KimJunSeob <wnstjq616@gmail.com>
2019-10-31 07:14:57 +0900

    Updates

---
+++ precache-manifest.8ed6f240e956790d13de415f65960266.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0c0abfd9ab99a34dd6e5606748bdd2",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "03ca003b942713724369",
+    "url": "/movie-app/static/css/main.43ae85b1.chunk.css"
+  },
+  {
+    "revision": "062d7dfeeff7cff8b995",
+    "url": "/movie-app/static/js/2.c1f4d69a.chunk.js"
+  },
+  {
+    "revision": "03ca003b942713724369",
+    "url": "/movie-app/static/js/main.bd306ce5.chunk.js"
+  },
+  {
+    "revision": "9cdb516fe76810f2c8f7",
+    "url": "/movie-app/static/js/runtime-main.19e7ea9e.js"
+  }
+]);
