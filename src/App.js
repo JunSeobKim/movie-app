6febf651d41f6c9b515cb6838646877b95b267e9
MainDesktop_KimJunSeob <wnstjq616@gmail.com>
2019-10-30 11:53:32 +0900

    Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
 import React from "react";
-import propTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
 
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
-  componentDidMount() {
-    console.log("conponentDidMount");
-  }
-  componentDidUpdate() {
-    console.log("componentDidUpdate");
+  // getMovies = async()=>{
+  //   const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  // }
+
+  async componentDidMount() {
+    // this.getMovies();
+    const {
+      data: {
+        data: { movies }
+      } // es6
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    ); // json형태의 영화값들을 다 불러오고 data.data.movies안의 20개의 영화 리스트를 저장
+    this.setState({ movies, isLoading: false }); // state 안의 movies 에 넣고 isLoading 값을 false로 변경
   }
 
   render() {
-    console.log("render");
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        <h1>the number is: {this.state.count}</h1>
-        <button onClick={this.add}>add</button>
-        <button onClick={this.minus}>minus</button>
-      </div>
+      <section class="container">
+        {isLoading ? ( // componentDidMount를 다 처리할 때 까지 삼항연산자로 Loading을 화면에 보여줌 * lifecycle : render -> componentDidMount
+          <div>Loading...</div>
+        ) : (
+          <div class="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image} // Movie.js의 틀을 사용해 값을 나타낸다.
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
